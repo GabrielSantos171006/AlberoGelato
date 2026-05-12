8aa9d0ddd6eac10b8a79a7454ec9f80d25193125
pedropizanee23 <10753060@mackenzista.com.br>
2026-05-12 23:00:34 +0000

    header

--- borboleta/src/app/src/components/Header.js
+++ borboleta/src/app/src/components/Header.js
@@ -1,1 +1,11 @@
-
+export default function Header() {
+  return (
+    <header>
+      <div><strong>Gelato</strong></div>
+      <nav>
+        <a href="#sobre">Sobre nós</a>
+        <a href="#produtos">Produtos</a>
+      </nav>
+    </header>
+  );
+}
