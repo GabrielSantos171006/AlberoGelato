7859466963a6354fa2e097b3bb2b206c33fbaa15
pedropizanee23 <10753060@mackenzista.com.br>
2026-05-12 20:32:55 -0300

    Update SecaoProdutos.js

--- borboleta/src/app/src/components/SecaoProdutos.js
+++ borboleta/src/app/src/components/SecaoProdutos.js
@@ -2,12 +2,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import styles from '../app/page.module.css'; // Ajuste o caminho do CSS se necessário
-
-export const produtosData = [
-  { id: 1, category: 'fruta', img: 'https://revistaanamaria.com.br/wp-content/uploads/2024/02/gelato-de-morango.jpg', nome: 'Morango' },
-  { id: 2, category: 'chocolate', img: 'https://chefrichardrobert.wordpress.com/wp-content/uploads/2018/11/sorv.jpg', nome: 'Chocolate Belga' },
-  { id: 3, category: 'fruta', img: 'https://panelaterapia.com/wp-content/uploads/2012/12/manga.png', nome: 'Manga' }
-];
+import { produtosData } from '../data/produtos';
 
 export default function SecaoProdutos() {
   const [filtroAtivo, setFiltroAtivo] = useState('all');
